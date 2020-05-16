65689ee5cfa0d32e62024fa589e46971ad198fb7
Leah Silber <LeahSilber@gmail.com>
2020-05-16 16:56:56 -0700

    Done.

--- src/components/Keypad.js
+++ src/components/Keypad.js
@@ -1,1 +1,21 @@
-// Code Keypad Component Here
+// @ts-check
+
+import React, { Component } from "react";
+
+export default class Keypad extends Component {
+  handleClick = (event) => {
+    {
+      console.log("Entering password...");
+    }
+  };
+
+  render() {
+    return (
+      <div>
+        <input type="password" onKeyUp={this.handleClick}>
+          Something something
+        </input>
+      </div>
+    );
+  }
+}
--- src/index.js
+++ src/index.js
@@ -1,13 +1,15 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
+// @ts-check
 
-import Keypad from './components/Keypad';
-import EyesOnMe from './components/EyesOnMe';
+import React from "react";
+import ReactDOM from "react-dom";
+
+import Keypad from "./components/Keypad";
+import EyesOnMe from "./components/EyesOnMe";
 
 ReactDOM.render(
   <div>
     <Keypad />
     <EyesOnMe />
   </div>,
-  document.getElementById('global')
+  document.getElementById("global")
 );
